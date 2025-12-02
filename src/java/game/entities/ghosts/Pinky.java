e689d1a7833354f686110f04da3897b8b757ffce
Salvatore Palmeri <143839782+salpalmeri@users.noreply.github.com>
2025-12-02 12:42:33 -0500

    Update Pinky.java

--- src/java/game/entities/ghosts/Pinky.java
+++ src/java/game/entities/ghosts/Pinky.java
@@ -1,5 +1,6 @@
 package game.entities.ghosts;
 
+import game.ghostStates.PinkyChaseMode;
 import game.ghostStrategies.PinkyStrategy;
 
 //Classe concrète de Pinky (le fantôme rose)
@@ -7,5 +8,9 @@ public class Pinky extends Ghost {
     public Pinky(int xPos, int yPos) {
         super(xPos, yPos, "pinky.png");
         setStrategy(new PinkyStrategy());
+
+        //Use A* strategy instead of default
+        this.chaseMode = new PinkyChaseMode(this);
     }
 }
+
